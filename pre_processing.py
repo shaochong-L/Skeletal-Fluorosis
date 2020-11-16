447c2086626c493c63af7e583b35e2f49a42cf31
shaochong-L <71553451+shaochong-L@users.noreply.github.com>
2020-11-16 17:55:56 +0800

    Add files via upload

---
+++ pre_processing.py
@@ -0,0 +1,47 @@
+import numpy as np
+from PIL import Image
+import cv2
+import configparser
+
+def my_PreProc(data):
+    assert(len(data.shape)==4)
+    assert (data.shape[1]==3)
+    train_imgs = rgb2gray(data)
+    train_imgs = dataset_normalized(train_imgs)
+    train_imgs = clahe_equalized(train_imgs)
+    train_imgs = adjust_gamma(train_imgs, 1.2)
+    train_imgs = train_imgs/255.
+    return train_imgs
+def rgb2gray(rgb: object) -> object:
+    assert (len(rgb.shape)==4)  #4D arrays
+    assert (rgb.shape[1]==3)
+    bn_imgs = rgb[:,0,:,:]*0.299 + rgb[:,1,:,:]*0.587 + rgb[:,2,:,:]*0.114
+    bn_imgs = np.reshape(bn_imgs,(rgb.shape[0],1,rgb.shape[2],rgb.shape[3]))
+    return bn_imgs
+def dataset_normalized(imgs: object) -> object:
+    assert (len(imgs.shape)==4)  #4D arrays
+    imgs_normalized = np.empty(imgs.shape)
+    imgs_std = np.std(imgs)
+    imgs_mean = np.mean(imgs)
+    imgs_normalized = (imgs-imgs_mean)/imgs_std
+    for i in range(imgs.shape[0]):
+        imgs_normalized[i] = ((imgs_normalized[i] - np.min(imgs_normalized[i])) / (np.max(imgs_normalized[i])-np.min(imgs_normalized[i])))*255
+    return imgs_normalized
+def clahe_equalized(imgs):
+    assert (len(imgs.shape)==4)  #4D arrays
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    imgs_equalized = np.empty(imgs.shape)
+    for i in range(imgs.shape[0]):
+        imgs_equalized[i,0] = clahe.apply(np.array(imgs[i,0], dtype = np.uint8))
+    return imgs_equalized
+def adjust_gamma(imgs, gamma=1.0):
+    assert (len(imgs.shape)==4)  #4D arrays
+    invGamma = 1.0 / gamma
+    table = np.array([((i / 255.0) ** invGamma) * 255 for i in np.arange(0, 256)]).astype("uint8")
+    new_imgs = np.empty(imgs.shape)
+    for i in range(imgs.shape[0]):
+        new_imgs[i,0] = cv2.LUT(np.array(imgs[i,0], dtype = np.uint8), table)
+    return new_imgs
+
+
+img = cv2.imread()
